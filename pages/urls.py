4f748a04d221f07ad419b90dc3772975974d16fe
Patrick Kernan <pkernan18@yahoo.com>
2026-05-26 15:12:35 -0400

    attempted bugfix

--- pages/urls.py
+++ pages/urls.py
@@ -1,12 +1,9 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
-from django.contrib import admin
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('services/', views.services, name='services'),
     path('contact/', views.contact, name='contact'),
-    path('admin/', admin.site.urls),
-    path('', include('pages.urls')),
 ]
